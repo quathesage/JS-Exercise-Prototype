7bf5b0dfaec3770297f60c42f5beeb1cdac3cffc
quavo <qua.thesage19@gmail.com>
2021-10-13 15:30:33 -0500

    finished task 3

--- index.js
+++ index.js
@@ -87,6 +87,11 @@ Car.prototype.fill = function(gallons){
   this.tank = gallons + this.tank;
 }
 
+// Car.prototype.drive = function(distance){
+//   this.odometer = this.odometer + distance;
+//   this.tank = 
+// }
+
 /*
   TASK 3
     - Write a Baby constructor subclassing Person.
@@ -94,10 +99,14 @@ Car.prototype.fill = function(gallons){
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
- 
+function Baby(name, age, favoriteToy) {
+   Person.call(this, name, age, favoriteToy);
+   this.favoriteToy = favoriteToy;
 }
-
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`;
+}
 
 /* 
   TASK 4
