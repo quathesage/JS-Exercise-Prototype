802f7534bac40a8f5d3602e7b1ccb0a8846d9097
quavo <qua.thesage19@gmail.com>
2021-10-13 14:44:14 -0500

    Completed task 1

--- index.js
+++ index.js
@@ -39,16 +39,29 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-  
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
 
+Person.prototype.eat = function(edible){
+  if(this.stomach.length < 10){
+      this.stomach.push(edible);
+  }
+}
 
+Person.prototype.poop = function(){
+  return this.stomach = [];
+}
 
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`
+}
 
+const dequavion = new Person("dequavion", 23);
 
-
-
+console.log(dequavion.toString());
 /*
   TASK 2
     - Write a Car constructor that initializes `model` and `milesPerGallon` from arguments.
